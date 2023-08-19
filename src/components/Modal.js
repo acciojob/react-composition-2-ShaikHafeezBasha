1e95de81952c586891b2ba0f4e67111a24764469
Hafeez Basha <shaikhafeez500@gmail.com>
2023-08-19 23:15:24 +0530

    submitting code-Sat Aug 19 2023 23:14:14 GMT+0530 (India Standard Time)

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -4,16 +4,16 @@ const Modal = ({ showprop, setShowprop }) => {
         setShowprop(false)
     }
     return (
-        <div className="model">
+        <>
             <button onClick={() => { setShowprop(true) }}>Show Modal</button>
             {
                 showprop &&
                 <div className="model-overlay" onClick={onClose}>
                     <button className="model-close" onClick={onClose}>Close</button>
-                    <p className="model-p">This is the content of the modal</p>
+                    <p className="model-p">This is the content of the modal.</p>
                 </div>
             }
-        </div>
+        </>
     )
 }
 export default Modal
